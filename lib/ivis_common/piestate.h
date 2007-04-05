devurandom/warzone2100

commit 762eab708909897909c64b43c06104cd6a8dbb2b

Tweaks and optimisations for CPU usage. Tiles are no longer added to the bucket sort, and the size of the bucket array is made smaller. The maximum number of simultaneous effects is down from 2500 to 500 (a big fight has about 200 effects). The effect processing and drawing is now done from one loop instead of from 2 (saves on the iterations). The pie_DrawTexTriangle function defaults to non transparent tiles and sets an alpha drawing mode only when needed.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1286 4a71c877-e1ca-e34f-864e-861f7616d084

src/display3d.c
@@ -930,20 +930,12 @@ static void drawTiles(iView *camera, iView *player)
 	/* ---------------------------------------------------------------- */
 	/* Draw all the tiles or add them to bucket sort                     */
 	/* ---------------------------------------------------------------- */
-	tilesRejected = 0;
 	for (i= 0; i < (SDWORD)visibleYTiles; i++)
-
 	{
 		for (j= 0; j < (SDWORD)visibleXTiles; j++)
 		{
-#ifndef BUCKET
-//bucket?
-		drawTexturedTile(i,j);
-#else
-
-		if(tileScreenInfo[i][j].drawInfo == TRUE)
+			if(tileScreenInfo[i][j].drawInfo == TRUE)
 			{
-
 				tileIJ[i][j].i = i;
 				tileIJ[i][j].j = j;
 				//get distance of furthest corner
@@ -955,14 +947,14 @@ static void drawTiles(iView *camera, iView *player)
 				{
 					ASSERT( FALSE,"Weirdy tile coords" );
 				}
-				bucketAddTypeToList(RENDER_TILE, &tileIJ[i][j]);
-				bucketAddTypeToList(RENDER_WATERTILE, &tileIJ[i][j]);
+				if(zMax<0)
+				{
+					// clipped
+					continue;
+				}
+				drawTerrainTile(i,j, FALSE);
+				drawTerrainWaterTile(i,j);
 			}
-			else
-			{
-				tilesRejected++;
-			}
-#endif //bucket
 		}
 	}
 
@@ -981,7 +973,8 @@ static void drawTiles(iView *camera, iView *player)
 	displayDelivPoints();	//bucket render implemented
 	display3DProjectiles();//bucket render implemented
 
-	drawEffects();
+	// Gerard: Now done in processEffects
+	// drawEffects();
 	atmosDrawParticles();
 #ifdef BUCKET
 	bucketRenderCurrentList();

lib/ivis_opengl/piedraw.c
@@ -1093,13 +1093,7 @@ void pie_DrawTexTriangle(PIEVERTEX *aVrts, SDWORD texPage, void* psEffects)
 	tileCount++;
 	pie_SetTexturePage(texPage);
 	pie_SetFogStatus(TRUE);
-	if (psEffects == NULL)
-	{
-		/* Solid terrain */
-		pie_SetRendMode(REND_GOURAUD_TEX);
-		pie_SetColourKeyedBlack(TRUE);
-	}
-	else
+	if (psEffects != NULL)
 	{
 		/* Translucent water with animation */
 		pie_SetRendMode(REND_ALPHA_TEX);
@@ -1116,6 +1110,12 @@ void pie_DrawTexTriangle(PIEVERTEX *aVrts, SDWORD texPage, void* psEffects)
 		glVertex3f( aVrts[i].sx, aVrts[i].sy, aVrts[i].sz );
 	}
 	glEnd();
+	if (psEffects != NULL)
+	{
+		/* Solid terrain */
+		pie_SetRendMode(REND_GOURAUD_TEX);
+		pie_SetColourKeyedBlack(TRUE);
+	}
 }
 
 void pie_GetResetCounts(SDWORD* pPieCount, SDWORD* pTileCount, SDWORD* pPolyCount, SDWORD* pStateCount)

src/bucket3d.c
@@ -44,8 +44,8 @@
 #include "atmos.h"
 
 
-#define NUM_BUCKETS		8000
-#define NUM_OBJECTS		4000
+#define NUM_BUCKETS		1000
+#define NUM_OBJECTS		1000
 #define BUCKET_OFFSET	0
 #define BUCKET_RANGE	32000
 

src/effects.c
@@ -81,7 +81,8 @@
 extern UWORD OffScreenEffects;
 
 /* Our list of all game world effects */
-static EFFECT	asEffectsList[MAX_EFFECTS];
+static EFFECT        asEffectsList[MAX_EFFECTS];
+static EFFECT_STATUS effectStatus[MAX_EFFECTS];
 
 #define FIREWORK_EXPLODE_HEIGHT			400
 #define STARBURST_RADIUS				150
@@ -199,6 +200,12 @@ UDWORD IMDGetNumFrames(iIMDShape *Shape);
 /* The fraction of a second that the last game frame took */
 static	FRACT	fraction;
 
+static void killEffect(EFFECT *e)
+{
+	effectStatus[e-asEffectsList] = ES_INACTIVE;
+	e->control = (UBYTE) 0;
+}
+
 // ----------------------------------------------------------------------------------------
 static BOOL	essentialEffect(EFFECT_GROUP group, EFFECT_TYPE type)
 {
@@ -270,7 +277,7 @@ EFFECT	*psEffect;
 		/* Clear all the control bits */
 		psEffect->control = (UBYTE)0;
 		/* All effects are initially inactive */
-		asEffectsList[i].status = ES_INACTIVE;
+		effectStatus[i] = ES_INACTIVE;
 	}
 }
 
@@ -509,7 +516,7 @@ void	addEffect(Vector3i *pos, EFFECT_GROUP group, EFFECT_TYPE type,BOOL specifie
 	}
 
 	/* Make the effect active */
-	asEffectsList[freeEffect].status = ES_ACTIVE;
+	effectStatus[freeEffect] = ES_ACTIVE;
 
 	/* As of yet, it hasn't bounced (or whatever)... */
 	if(type!=EXPLOSION_TYPE_LAND_LIGHT)
@@ -569,25 +576,27 @@ UDWORD	num;
 
 	/* Establish how long the last game frame took */
 	fraction = MAKEFRACT(frameTime)/GAME_TICKS_PER_SEC;
-	num=0;
 	missCount = 0;
 
+	/* Reset counter */
+	numEffects = 0;
+
+	/* Traverse the list */
 	for(i=0; i<MAX_EFFECTS; i++)
 	{
-		/* Is it active */
-		switch(asEffectsList[i].status)
+		/* Don't bother unless it's active */
+		if(effectStatus[i] == ES_ACTIVE)
 		{
-		/* The effect is active */
-		case ES_ACTIVE:
-			/* So process it */
 			updateEffect(&asEffectsList[i]);
-			num++;
-			break;
-		case ES_DORMANT:
-			/* Might be useful? */
-			break;
-		default:
-			break;
+			/* One more is active */
+			numEffects++;
+			/* Is it on the grid */
+			if(clipXY((UDWORD)MAKEINT(asEffectsList[i].position.x),(UDWORD)MAKEINT(asEffectsList[i].position.z)))
+			{
+				/* Add it to the bucket */
+				bucketAddTypeToList(RENDER_EFFECT,&asEffectsList[i]);
+			}
+
 		}
 	}
 
@@ -597,7 +606,7 @@ UDWORD	num;
 	/* Add any structure effects */
 	effectStructureUpdates();
 
-	activeEffects = num;
+	activeEffects = numEffects;
 	skippedEffects = skipped;
 }
 
@@ -611,31 +620,7 @@ void	drawEffects( void )
 {
 UDWORD	i;
 
-	/* Reset counter */
-	numEffects = 0;
 
-	/* Traverse the list */
-	for(i=0; i<MAX_EFFECTS; i++)
-	{
-		/* Don't bother unless it's active */
-		if(asEffectsList[i].status == ES_ACTIVE)
-		{
-			/* One more is active */
-			numEffects++;
-			/* Is it on the grid */
-			if(clipXY((UDWORD)MAKEINT(asEffectsList[i].position.x),(UDWORD)MAKEINT(asEffectsList[i].position.z)))
-			{
-#ifndef BUCKET
-				/* Draw it right now */
-				renderEffect(&asEffectsList[i]);
-#else
-				/* Add it to the bucket */
-				bucketAddTypeToList(RENDER_EFFECT,&asEffectsList[i]);
-#endif
-			}
-
-		}
-	}
 }
 
 
@@ -703,7 +688,7 @@ void	updateWaypoint(EFFECT *psEffect)
 	if(!(keyDown(KEY_LCTRL) || keyDown(KEY_RCTRL) ||
 	     keyDown(KEY_LSHIFT) || keyDown(KEY_RSHIFT)))
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 	}
 }
 
@@ -780,7 +765,7 @@ void	updateFirework(EFFECT *psEffect)
 	//   			addEffect(&dv,EFFECT_FIREWORK, FIREWORK_TYPE_STARBURST,FALSE,NULL,0);
 				}
 			}
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 
 		}
 		else
@@ -813,7 +798,7 @@ void	updateFirework(EFFECT *psEffect)
 				else
 				{
 					/* Kill it off */
- 					KILL_EFFECT(psEffect);
+ 					killEffect(psEffect);
 					return;
 				}
 			}
@@ -826,7 +811,7 @@ void	updateFirework(EFFECT *psEffect)
 			if(gameTime - psEffect->birthTime > psEffect->lifeSpan)
 			{
 				/* Kill it */
-				KILL_EFFECT(psEffect);
+				killEffect(psEffect);
 			}
 		}
 
@@ -933,7 +918,7 @@ void	updateSatLaser(EFFECT *psEffect)
 	}
 	else
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 	}
 }
 // ----------------------------------------------------------------------------------------
@@ -1009,7 +994,7 @@ void	updateExplosion(EFFECT *psEffect)
 		if(psEffect->size>MAX_SHOCKWAVE_SIZE || light.range>600)
 		{
  			/* Kill it off */
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 
 		}
@@ -1027,7 +1012,7 @@ void	updateExplosion(EFFECT *psEffect)
 		 		if(psEffect->type!=EXPLOSION_TYPE_LAND_LIGHT)
 				{
 		 			/* Kill it off */
-					KILL_EFFECT(psEffect);
+					killEffect(psEffect);
 					return;
 				}
 				else
@@ -1060,7 +1045,7 @@ void	updateBlood(EFFECT *psEffect)
 		if(++psEffect->frameNumber >= EffectGetNumFrames(psEffect))
 		{
 			/* Kill it off */
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 		}
 	}
@@ -1104,7 +1089,7 @@ void	updatePolySmoke(EFFECT *psEffect)
 			else
 			{
 				/* Kill it off */
- 				KILL_EFFECT(psEffect);
+ 				killEffect(psEffect);
 				return;
 			}
 		}
@@ -1124,7 +1109,7 @@ void	updatePolySmoke(EFFECT *psEffect)
 		if(gameTime - psEffect->birthTime > psEffect->lifeSpan)
 		{
 			/* Kill it */
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 		}
 	}
 }
@@ -1169,7 +1154,7 @@ void	updateGraviton(EFFECT *psEffect)
 	if(((UDWORD)MAKEINT(psEffect->position.x)/TILE_UNITS >= mapWidth) ||
 		(UDWORD)MAKEINT(psEffect->position.z)/TILE_UNITS >= mapHeight)
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 		return;
 	}
 
@@ -1178,7 +1163,7 @@ void	updateGraviton(EFFECT *psEffect)
 	/* If it's going up and it's still under the landscape, then remove it... */
 	if(psEffect->position.y<groundHeight && MAKEINT(psEffect->velocity.y)>0)
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 		return;
 	}
 
@@ -1232,7 +1217,7 @@ void	updateGraviton(EFFECT *psEffect)
 	if((MAKEINT(psEffect->position.x) <= TILE_UNITS) ||
 		MAKEINT(psEffect->position.z) <= TILE_UNITS)
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 		return;
 	}
 
@@ -1242,7 +1227,7 @@ void	updateGraviton(EFFECT *psEffect)
 		psTile = mapTile((MAKEINT(psEffect->position.x))>>TILE_SHIFT,(MAKEINT(psEffect->position.z))>>TILE_SHIFT);
 	   	if(TERRAIN_TYPE(psTile) == TER_WATER)
 		{
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 		}
 		else
@@ -1273,7 +1258,7 @@ void	updateGraviton(EFFECT *psEffect)
 					dv.z = MAKEINT(psEffect->position.z);
 					addEffect(&dv,EFFECT_EXPLOSION,EXPLOSION_TYPE_VERY_SMALL,FALSE,NULL,0);
 				}
-				KILL_EFFECT(psEffect);
+				killEffect(psEffect);
 				return;
 			}
 		}
@@ -1330,7 +1315,7 @@ void	updateDestruction(EFFECT *psEffect)
 	if(gameTime > (psEffect->birthTime + psEffect->lifeSpan))
 	{
 		/* Kill it - it's too old */
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 		return;
 	}
 
@@ -1343,7 +1328,7 @@ void	updateDestruction(EFFECT *psEffect)
 			pos.z = MAKEINT(psEffect->position.z);
 			pos.y = MAKEINT(psEffect->position.y);
 			addEffect(&pos,EFFECT_EXPLOSION,EXPLOSION_TYPE_LARGE,FALSE,NULL,0);
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 		}
 
@@ -1484,7 +1469,7 @@ void	updateConstruction(EFFECT *psEffect)
 			}
 			else
 			{
-				KILL_EFFECT(psEffect);
+				killEffect(psEffect);
 				return;
 			}
 		}
@@ -1504,13 +1489,13 @@ void	updateConstruction(EFFECT *psEffect)
 		if(MAKEINT(psEffect->position.y) <=
 			map_Height((UDWORD)MAKEINT(psEffect->position.x),(UDWORD)MAKEINT(psEffect->position.z)))
 		{
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 		}
 
 		if(gameTime - psEffect->birthTime > psEffect->lifeSpan)
 		{
-			KILL_EFFECT(psEffect);
+			killEffect(psEffect);
 			return;
 		}
 	}
@@ -1587,7 +1572,7 @@ void	updateFire(EFFECT *psEffect)
 
 	if(gameTime - psEffect->birthTime > psEffect->lifeSpan)
 	{
-		KILL_EFFECT(psEffect);
+		killEffect(psEffect);
 		return;
 	}
 }
@@ -2898,7 +2883,7 @@ BOOL	bOnFire;
 
    	for(i=0, bOnFire = FALSE; i<MAX_EFFECTS && !bOnFire; i++)
 	{
-	 	if( (asEffectsList[i].status == ES_ACTIVE) && asEffectsList[i].group == EFFECT_FIRE)
+	 	if( (effectStatus[i] == ES_ACTIVE) && asEffectsList[i].group == EFFECT_FIRE)
 		{
 			posX = MAKEINT(asEffectsList[i].position.x);
 			posY = MAKEINT(asEffectsList[i].position.z);
@@ -2926,7 +2911,7 @@ iIMDShape		*psOrig;
 	/* How many FX do we write out data from? Only write active ones! */
 	for(i=0,fxEntries = 0; i<MAX_EFFECTS; i++)
 	{
-		if(asEffectsList[i].status == ES_ACTIVE)
+		if(effectStatus[i] == ES_ACTIVE)
 		{
 			fxEntries++;
 		}
@@ -2967,7 +2952,7 @@ iIMDShape		*psOrig;
 
 	for(i=0; i<MAX_EFFECTS; i++)
 	{
-		if(asEffectsList[i].status == ES_ACTIVE)
+		if(effectStatus[i] == ES_ACTIVE)
 		{
 			/*
 			restore = FALSE;

src/effects.h
@@ -120,7 +120,7 @@ typedef enum
 }LAND_LIGHT_SPEC;
 
 
-#define MAX_EFFECTS	2500
+#define MAX_EFFECTS	500
 
 #define	GRAVITON_GRAVITY	((FRACT)-800)
 #define	EFFECT_X_FLIP		0x1
@@ -195,7 +195,6 @@ typedef enum
 
 typedef struct	_effect_def
 {
-UBYTE			status;			// what status is the present effect - active/inactive/dormant
 UBYTE			control;		// Controls the bits above - essential,flips etc
 UBYTE			group;			// what	group is it - explosion, building effect etc....
 UBYTE			type;			// what type is it within the group?
@@ -214,9 +213,6 @@ UWORD			lifeSpan;		// what is it's life expectancy?
 UWORD			radius;			// Used for area effects
 struct iIMDShape		*imd;			// pointer to the imd the effect uses.
 } EFFECT;
-
-#define KILL_EFFECT(x)	(x)->status = ES_INACTIVE; \
-						(x)->control = (UBYTE) 0
 
 /* Maximum number of effects in the world - need to investigate what this should be */
 /* EXTERNAL REFERENCES */

src/mapgrid.c
@@ -294,7 +294,7 @@ void gridStartIterate(SDWORD x, SDWORD y)
 
 // get the next object that could affect a location,
 // should only be called after gridStartIterate
-BASE_OBJECT *gridIterate(void)
+inline BASE_OBJECT *gridIterate(void)
 {
 	BASE_OBJECT		*psRet;
 

commit 439f1d970d9199a7c168100ae74d4124f9ff3d81

Fix some problems introduced in r1286: 1. flickering water edges 2. transparent water was not correctly drawn
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1363 4a71c877-e1ca-e34f-864e-861f7616d084

lib/ivis_common/piestate.h
@@ -156,6 +156,7 @@ extern SDWORD	pieStateCount;
 /***************************************************************************/
 extern void pie_SetDefaultStates(void);//Sets all states
 extern void pie_SetDepthBufferStatus(DEPTH_MODE depthMode);
+extern void pie_SetDepthOffset(float offset);
 extern void pie_SetGammaValue(float val);
 //renderer capability
 extern void pie_SetFogCap(FOG_CAP val);

lib/ivis_opengl/piestate.c
@@ -75,6 +75,21 @@ void pie_SetDepthBufferStatus(DEPTH_MODE depthMode) {
 	}
 }
 
+/// Set the depth (z) offset
+/// Negative values are closer to the screen
+void pie_SetDepthOffset(float offset)
+{
+	if(offset == 0.0f)
+	{
+		glDisable (GL_POLYGON_OFFSET_FILL);
+	}
+	else
+	{
+		glPolygonOffset(offset, offset);
+		glEnable (GL_POLYGON_OFFSET_FILL);
+	}
+}
+
 /// Set the OpenGL fog start and end
 void pie_UpdateFogDistance(float begin, float end)
 {

src/display3d.c
@@ -952,7 +952,18 @@ static void drawTiles(iView *camera, iView *player)
 					continue;
 				}
 				drawTerrainTile(i,j, FALSE);
-				drawTerrainWaterTile(i,j);
+				if(tileScreenInfo[i][j].bWater)
+				{
+					// add the (possibly) transparent water to the bucket sort
+					bucketAddTypeToList(RENDER_WATERTILE, &tileIJ[i][j]);
+					// check if we need to draw a water edge
+					if((mapTile(playerXTile + j, playerZTile + i)->texture & TILE_NUMMASK) != WaterTileID) {
+						// the edge is in front of the water (which is drawn at z-index -1)
+						pie_SetDepthOffset(-2.0);
+						drawTerrainTile(i,j, TRUE);
+						pie_SetDepthOffset(0.0);
+					}
+				}
 			}
 		}
 	}
@@ -4974,6 +4985,7 @@ void drawTerrainWaterTile(UDWORD i, UDWORD j)
 		aVrts[2].light = tileScreenInfo[i+1][j+1].wlight;
 		aVrts[2].light.byte.a = WATER_ALPHA_LEVEL;
 
+		pie_SetDepthOffset(-1.0);
 		pie_DrawTexTriangle(aVrts, tileTexInfo[tileNumber & TILE_NUMMASK].texPage, &waterRealValue);//jps 15 apr99
 
 		memcpy(&aVrts[1],&aVrts[2],sizeof(PIEVERTEX));
@@ -4987,9 +4999,7 @@ void drawTerrainWaterTile(UDWORD i, UDWORD j)
 
 		pie_DrawTexTriangle(aVrts, tileTexInfo[tileNumber & TILE_NUMMASK].texPage, &waterRealValue);//jps 15 apr99
 
-		if( (psTile->texture & TILE_NUMMASK) != WaterTileID) {
-			drawTerrainTile(i,j, TRUE);
-		}
+		pie_SetDepthOffset(0.0);
 	}
 }
 