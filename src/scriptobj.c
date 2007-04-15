devurandom/warzone2100

commit 7c06d7acf5db340e0e9be618867b15a44ac90fa5

Remove strange usage of hash names in soundtrack code. I am not sure why it worked, or, perhaps, why it still works. Take care ;-)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1251 4a71c877-e1ca-e34f-864e-861f7616d084

src/scriptobj.c
@@ -618,7 +618,7 @@ static char *scrGetStatName(INTERP_TYPE type, UDWORD data)
 BOOL scrValDefSave(INTERP_VAL *psVal, char *pBuffer, UDWORD *pSize)
 {
 	VIEWDATA	*psIntMessage;
-	char		*pName;
+	const char	*pName;
 	RESEARCH	*psResearch;
 	char		*pPos;
 	DROID		*psCDroid;
@@ -812,23 +812,17 @@ BOOL scrValDefSave(INTERP_VAL *psVal, char *pBuffer, UDWORD *pSize)
 		*pSize = sizeof(UDWORD) * members + sizeof(SDWORD) * 5;
 		break;
 	case ST_SOUND:
-/*		pName = sound_GetTrackName((SDWORD)data);
+		pName = sound_GetTrackName((UDWORD)psVal->v.ival);
 		if (pName == NULL)
 		{
-			DBERROR(("scrValDefSave: couldn't get sound track name"));
+			debug(LOG_ERROR, "scrValDefSave: couldn't get sound track name");
 			return FALSE;
 		}
 		if (pBuffer)
 		{
-			strcpy((char *)pBuffer, pName);
+			strcpy(pBuffer, pName);
 		}
-		*pSize = strlen((char *)pName) + 1;*/
-		if (pBuffer)
-		{
-			*((UDWORD *) pBuffer) = sound_GetTrackHashName((SDWORD)psVal->v.ival);
-			endian_udword((UDWORD*)pBuffer);
-		}
-		*pSize = sizeof(UDWORD);
+		*pSize = strlen(pName) + 1;
 		break;
 	case ST_STRUCTUREID:
 	case ST_DROIDID:

lib/sound/track.c
@@ -213,9 +213,6 @@ TRACK *sound_LoadTrackFromBuffer(char *pBuffer, UDWORD udwSize)
 	}
 	strcpy( pTrack->pName, GetLastResourceFilename() );
 
-	// Set HASH of resource in filename
-	pTrack->resID = GetLastHashName();
-
 	if ( sound_ReadTrackFromBuffer(pTrack, pBuffer, udwSize) == FALSE )
 		return NULL;
 
@@ -246,7 +243,6 @@ BOOL sound_LoadTrackFromFile(char szFileName[])
 		}
 
 		strcpy( pTrack->pName, (char*) szFileName );
-		pTrack->resID = HashStringIgnoreCase( (char*) szFileName );
 		if ( sound_ReadTrackFromFile(pTrack, szFileName) == FALSE )
 		{
 			return FALSE;
@@ -413,18 +409,6 @@ const char *sound_GetTrackName( SDWORD iTrack )
 	if ( iTrack == SAMPLE_NOT_FOUND ) return NULL;
 	ASSERT( g_apTrack[iTrack] != NULL, "sound_GetTrackName: unallocated track" );
 	return g_apTrack[iTrack] ? g_apTrack[iTrack]->pName : "unallocated";
-}
-
-//*
-// =======================================================================================================================
-// =======================================================================================================================
-//
-UDWORD sound_GetTrackHashName( SDWORD iTrack )
-{
-	if (iTrack == 0 || iTrack == SAMPLE_NOT_FOUND)
-		return 0;
-	ASSERT( g_apTrack[iTrack] != NULL, "sound_GetTrackName: unallocated track" );
-	return g_apTrack[iTrack] ? g_apTrack[iTrack]->resID : 0;
 }
 
 //*

lib/sound/track.h
@@ -101,8 +101,6 @@ typedef struct TRACK
 	BOOL		bMemBuffer;				/* memory buffer flag       */
 	ALuint		iBufferName;				/* name of the openal buffer */
 	char		*pName;					// resource name of the track
-	UDWORD		resID;					// hashed name of the WAV
-
 } TRACK;
 
 /***************************************************************************/
@@ -130,7 +128,6 @@ SDWORD	sound_GetTrackPriority( SDWORD iTrack );
 SDWORD	sound_GetTrackAudibleRadius( SDWORD iTrack );
 SDWORD	sound_GetTrackVolume( SDWORD iTrack );
 const char *	sound_GetTrackName( SDWORD iTrack );
-UDWORD	sound_GetTrackHashName( SDWORD iTrack );
 
 BOOL	sound_TrackLooped( SDWORD iTrack );
 SDWORD	sound_TrackAudibleRadius( SDWORD iTrack );

commit 18ac935dc2b03daa72964b231b1b0ce3a727b411

* Fix bug #8938 (introduced in r1251) which occurred due to saving/loading functions of ST_SOUND being dissymmetric (save function stored the fileName, load function interpreted it as a UDWORD hash)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1470 4a71c877-e1ca-e34f-864e-861f7616d084

src/scriptobj.c
@@ -859,6 +859,7 @@ BOOL scrValDefLoad(SDWORD version, INTERP_VAL *psVal, char *pBuffer, UDWORD size
 	UDWORD			id;
 	LEVEL_DATASET	*psLevel;
 	DROID_GROUP		*psGroup;
+	const char              *pName;
 
 	switch (psVal->type)
 	{
@@ -1144,21 +1145,17 @@ BOOL scrValDefLoad(SDWORD version, INTERP_VAL *psVal, char *pBuffer, UDWORD size
 		break;
 	case ST_SOUND:
 		// find audio id
-		id = *((UDWORD *) pBuffer);
-		endian_udword(&id);
-		index = audio_GetTrackIDFromHash( id );
+		pName = pBuffer;
+		index = audio_GetTrackID( pName );
 		if (index == SAMPLE_NOT_FOUND)
 		{
-			// find empty id
-			index = audio_GetAvailableID();
-			if (index == SAMPLE_NOT_ALLOCATED)
+			// find empty id and set track vals
+			if (audio_SetTrackVals( pName, FALSE, &index, 100, 1800 ))
 			{
-				debug( LOG_ERROR, "Sound ID not available %s not found", pBuffer );
+				debug( LOG_ERROR, "Sound ID not available %s not found", pName );
 				abort();
 				break;
 			}
-			// set track vals
-			audio_SetTrackValsHashName( id, FALSE, index, 100, 1800 );
 		}
 		psVal->v.ival = index;
 		break;