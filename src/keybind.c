devurandom/warzone2100

commit 190b3eb115a4bb3bc84a7cbce17f03e9892e5676

Closes ticket #131 Adds weapon range display to the sensor display routine. Function was split for future enhancements.
This is *not* meant to be played with it enable all the time, as you know, it is a severe resource hog, and it is mainly helpful for modders, to check what the range of the weapons & sensors are.

NOTE: it only shows range for *first* weapon slot.


git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@6566 4a71c877-e1ca-e34f-864e-861f7616d084

src/keybind.c
@@ -194,7 +194,7 @@ void kf_ToggleSensorDisplay( void )
 	if (rangeOnScreen)
 		addConsoleMessage(_("Lets us see what you see!"), LEFT_JUSTIFY, SYSTEM_MESSAGE);        //added this message... Yeah, its lame. :)
 	else
-		addConsoleMessage(_("Fine, sensor display is off!"), LEFT_JUSTIFY, SYSTEM_MESSAGE);     //added this message... Yeah, its lame. :)
+		addConsoleMessage(_("Fine, weapon & sensor display is off!"), LEFT_JUSTIFY, SYSTEM_MESSAGE);     //added this message... Yeah, its lame. :)
 }
 //===================================================
 /* Halves all the heights of the map tiles */
@@ -1163,13 +1163,13 @@ void	kf_ToggleGodMode( void )
 
 	if(godMode)
 	{
-		FEATURE* psFeat;
+		FEATURE	*psFeat = apsFeatureLists[0];
 		int player;
+		STRUCTURE *psStruct;
 
 		godMode = false;
 		setRevealStatus(game.fog);
 		// now hide the features
-		psFeat = apsFeatureLists[0];
 		while (psFeat)
 		{
 			psFeat->visible[selectedPlayer] = 0;

src/display3d.c
@@ -4280,7 +4280,52 @@ STRUCTURE	*psStruct;
 	}//end if we want to display...
 }
 
-/// Show the sensor range for the specified object
+// Shows the weapon (long) range of the object in question.
+// Note, it only does it for the first weapon slot!
+static void showWeaponRange(BASE_OBJECT *psObj)
+{
+	UDWORD	i;
+	DROID	*psDroid;
+	STRUCTURE	*psStruct;
+	WEAPON_STATS	*psStats;
+	SDWORD	radius;
+	SDWORD	xDif,yDif;
+	UDWORD	weaponRange;
+	Vector3i pos;
+
+	if ( psObj->type == OBJ_DROID)
+	{
+		psDroid = (DROID*)psObj;
+		psStats = asWeaponStats + psDroid->asWeaps[0].nStat;//weapon_slot
+		weaponRange = psStats->longRange;
+	}
+	else
+	{
+		psStruct = (STRUCTURE*)psObj;
+		if(psStruct->pStructureType->numWeaps == 0) return;
+		weaponRange = psStruct->pStructureType->psWeapStat[0]->longRange;
+	}
+
+	if ( weaponRange == 0)
+	{
+		return;		//don't bother if no range.
+	}
+
+	for(i=0; i<360; i+=23)
+	{
+		radius = weaponRange;
+		xDif = radius * (SIN(DEG(i)));
+		yDif = radius * (COS(DEG(i)));
+
+		xDif = xDif/4096;	 // 'cause it's fixed point
+		yDif = yDif/4096;
+		pos.x = MAX(psObj->pos.x - xDif, 0);
+		pos.z = MAX(psObj->pos.y - yDif, 0);
+		pos.y = map_Height(pos.x,pos.z) + 16;
+		effectGiveAuxVar(40);	// half normal plasma size...
+		addEffect(&pos,EFFECT_EXPLOSION,EXPLOSION_TYPE_VERY_SMALL,false,NULL,0);
+	}
+}
 static void	showSensorRange2(BASE_OBJECT *psObj)
 {
 	SDWORD	radius;
@@ -4290,9 +4335,8 @@ static void	showSensorRange2(BASE_OBJECT *psObj)
 	UDWORD	i;
 	DROID	*psDroid;
 	STRUCTURE	*psStruct;
-	BOOL	bBuilding=false;
 
-	for(i=0; i<360; i++)
+	for(i=0; i<360; i+=15)
 	{
 		if(psObj->type == OBJ_DROID)
 		{
@@ -4303,7 +4347,6 @@ static void	showSensorRange2(BASE_OBJECT *psObj)
 		{
 			psStruct = (STRUCTURE*)psObj;
 			sensorRange = structSensorRange(psStruct);
-			bBuilding = true;
 		}
 		radius = sensorRange;
 		xDif = radius * (SIN(DEG(i)));
@@ -4315,15 +4358,11 @@ static void	showSensorRange2(BASE_OBJECT *psObj)
 		pos.z = MAX(psObj->pos.y - yDif, 0);
 		pos.y = map_Height(pos.x,pos.z) + 16;
 		effectGiveAuxVar(80);	// half normal plasma size...
-		if(bBuilding)
-		{
-			addEffect(&pos,EFFECT_EXPLOSION,EXPLOSION_TYPE_SMALL,false,NULL,0);
-		}
-		else
-		{
-			addEffect(&pos,EFFECT_EXPLOSION,EXPLOSION_TYPE_LASER,false,NULL,0);
-		}
+
+		addEffect(&pos,EFFECT_EXPLOSION,EXPLOSION_TYPE_LASER,false,NULL,0);
 	}
+
+	showWeaponRange(psObj);
 }
 
 /// Draw a circle on the map (to show the range of something)

commit 06818db2bfbf163556406332fa0243931670dbc1

Remove unused variable
fixes #236, caused by r6566

git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@6567 4a71c877-e1ca-e34f-864e-861f7616d084

src/keybind.c
@@ -1165,7 +1165,6 @@ void	kf_ToggleGodMode( void )
 	{
 		FEATURE	*psFeat = apsFeatureLists[0];
 		int player;
-		STRUCTURE *psStruct;
 
 		godMode = false;
 		setRevealStatus(game.fog);